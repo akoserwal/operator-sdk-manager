f0c70f41e4f6bf45e41a022f5a3bfd757a4f93e8
akoserwa <akoserwa@redhat.com>
2020-04-28 19:11:26 +0530

    version bump for release

--- version/version.go
+++ version/version.go
@@ -8,6 +8,6 @@ import (
 
 //var needs to be used instead of const for ldflags
 var (
-	Version           = "v0.0.1"
+	Version           = "v0.0.2"
 	GoVersion         = fmt.Sprintf("%s %s/%s", runtime.Version(), runtime.GOOS, runtime.GOARCH)
 )
